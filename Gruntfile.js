deade8f14ad726b9f4ea363fc772dfb9e69af667
Gilles Bassière <gilles_bassiere@natural-solutions.eu>
2014-04-10 12:45:06 +0200

    initial grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+﻿module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: [
+                        'config.xml',
+                        'css/**/*.{png,svg,jpeg,jpg,gif,eot,ttf,woff}',
+                        'data/*.xml',
+                        'data/**/*.{jpg,png}',
+                        'libs/**/*.{png,svg,jpeg,jpg,gif,eot,ttf,woff}',
+                        'libs/**/*min.{js,css}',
+                        'tpl/*.html'
+                    ]
+                }]
+            }
+        },
+        cssmin: {
+            dist: {
+                src: ['www/css/*.css'],
+                dest: 'build/css/all.min.css'
+            },
+            libflexslider: {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['libs/flexslider/flexslider.css']
+                }]
+            },
+            libmmenu: {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['libs/jQuery.mmenu-master/src/css/jquery.mmenu.css']
+                }]
+            },
+            libnotification : {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['libs/NS.UI.Notification/*.css']
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['js/*.js']
+                }]
+            },
+            libforms : {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['libs/NS.UI.Forms/forms.js']
+                }]
+            },
+            libnotification : {
+                files: [{
+                    expand: true,
+                    cwd: 'www',
+                    dest: 'build',
+                    src: ['libs/NS.UI.Notification/*.js']
+                }]
+            }
+        },
+        replace: {
+            dist: {
+                src: ['www/index.html'],
+                dest: 'build/index.html',
+                options: {
+                    patterns: [{
+                        match: /css\/main\.css/,
+                        replacement: 'css/all.min.css'
+                    }, {
+                        match: /[\s\t]*<link .*css\/key\.css.*\/>/,
+                        replacement: ''
+                    }, {
+                        match: /(jquery-1\.9\.1|jquery\.mmenu)\.js/g,
+                        replacement: '$1.min.js'
+                    }, {
+                        match: /(underscore|backbone)\.js/g,
+                        replacement: '$1-min.js'
+                    }]
+                }
+            }
+        }
+        // TODO: replace
+        // TODO: zip + phonegap build : https://coderwall.com/p/e0jxea
+        // TODO: comment concaténer les templates (<script> wrapper + function de loading adaptée)
+        // TODO: concaténer les JS en gérant le chargement de Forms.js
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-replace');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'cssmin', 'uglify', 'replace']);
+};
--- README.md
+++ README.md
@@ -2,3 +2,21 @@ sauvage
 =======
 
 Mobile application for "sauvage de ma rue" citizen science project
+
+Install notes
+-------------
+
+Developement and build environment relies on Git and Node.js (>= 0.8). Please,
+ensure you have this tools installed on your computer.
+
+To build the project, run the following commands::
+
+    cd <project root>/
+    npm install
+    grunt
+
+If you encounter any error, please update Node/NPM to a recent stable release
+before trying anything else.
+
+This will make a production release of the code (minification, etc) and put it
+in the `build/` directory.
