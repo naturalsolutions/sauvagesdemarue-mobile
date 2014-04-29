8f140b6768fab563dea19fedea7ebc232d2bdfcc
cdcvidal <cdcvidal@yahoo.fr>
2014-04-29 19:31:09 +0200

    test si l'email est vailde avant l'envoi

--- www/js/view.js
+++ www/js/view.js
@@ -936,6 +936,7 @@ app.views.TaxonDetailView=  app.utils.BaseView.extend({
 								self.insertView("#values-list", new app.views.IKCriteriaListItemFilterTaxonView({model: criteria, taxon: self.model}));
 						}, this);
 				$('.page-title').replaceWith("<div class='page-title'>"+ this.model.get('commonName')+"</div><em>"+ this.model.get('scientificName')+"</em>");
+				$('.page-block-sub-title').empty();
   },
 
 		afterRender: function() {
@@ -961,6 +962,9 @@ app.views.TaxonDetailView=  app.utils.BaseView.extend({
 		remove: function(){
 				app.utils.BaseView.prototype.remove.apply(this, arguments);
 				$('.page-block-title em').remove();
+				if (app.globals.currentrue !== undefined) {
+						$('.page-sub-title').replaceWith("<h1 class='page-sub-title'>"+app.globals.currentrue.get('name') +" - "+app.globals.currentrue.get('cote') +"</h1>");
+				}
 				$('.navbar-fixed-bottom .btn-group .btn-footer').remove();
 		}
 });
@@ -1026,9 +1030,8 @@ app.views.ObservationListView =  app.utils.BaseView.extend({
 		},
 		validatorsEmail : function(value) {
 				var regex = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-						var flag = regex.test(value);
-						if (!flag)
-								return flag;
+				var flag = regex.test(value);
+				return flag;
 		},
   sendObs: function (event) {
     var connect = checkConnection();
@@ -1079,7 +1082,7 @@ app.views.ObservationListView =  app.utils.BaseView.extend({
 															"<button type='submit' id='submitEmail' class='btn btn-primary'>Valider</button>"+
 														"</form>"					
 													);
-												sauvages.notifications.email(msg());
+												sauvages.notifications.email(msg(),currentUser);
 												$('.modal-footer').addClass("hide");
 										}
 								}
